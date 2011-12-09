201493d9e4da8e62b027834c34a8dbce6dd8f582
yemao <leafy.myeh@allwinnertech.com>
2011-12-09 14:36:20 +0800

    mmc: add mmc uboot driver

---
+++ drivers/mmc/sunxi_mmc.c
@@ -0,0 +1,484 @@
+/*
+ * allwinenr's mmc driver for u-boot
+ * author: Aaron<leafy.myeh@allwinnertech.com>
+ * date: 2011-12-8 15:29:03
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/gpio.h>
+#include <asm/arch/cpu.h>
+#include <mmc.h>
+
+//#define SUNXI_MMCDBG
+
+#define SUNXI_SDMMC0_BASE	0x01C0F000
+#define SUNXI_SDMMC1_BASE	0x01C10000
+#define SUNXI_SDMMC2_BASE	0x01C11000
+#define SUNXI_SDMMC3_BASE	0x01C12000
+
+struct sw_mmc {
+	unsigned int 	 gctrl;			//(0x00) SMC Global Control Register
+	unsigned int 	 clkcr;         //(0x04) SMC Clock Control Register
+	unsigned int 	 timeout;       //(0x08) SMC Time Out Register
+	unsigned int 	 width;         //(0x0C) SMC Bus Width Register
+	unsigned int 	 blksz;         //(0x10) SMC Block Size Register
+	unsigned int 	 bytecnt;        //(0x14) SMC Byte Count Register
+	unsigned int 	 cmd;           //(0x18) SMC Command Register
+	unsigned int 	 arg;           //(0x1C) SMC Argument Register
+	unsigned int 	 resp0;         //(0x20) SMC Response Register 0
+	unsigned int 	 resp1;         //(0x24) SMC Response Register 1
+	unsigned int 	 resp2;         //(0x28) SMC Response Register 2
+	unsigned int 	 resp3;         //(0x2C) SMC Response Register 3
+	unsigned int 	 imask;         //(0x30) SMC Interrupt Mask Register
+	unsigned int 	 mint;          //(0x34) SMC Masked Interrupt Status Register
+	unsigned int 	 rint;          //(0x38) SMC Raw Interrupt Status Register
+	unsigned int 	 status;        //(0x3C) SMC Status Register
+	unsigned int 	 ftrglevel;     //(0x40) SMC FIFO Threshold Watermark Register
+	unsigned int 	 funcsel;       //(0x44) SMC Function Select Register
+	unsigned int 	 cbcr;          //(0x48) SMC CIU Byte Count Register
+	unsigned int 	 bbcr;          //(0x4C) SMC BIU Byte Count Register
+	unsigned int 	 dbgc;          //(0x50) SMC Debug Enable Register
+//	unsigned int 	 res0[11];		//(0x54~0x7c)
+//	unsigned int 	 dmac;          //(0x80) SMC IDMAC Control Register
+//	unsigned int 	 dlba;          //(0x84) SMC IDMAC Descriptor List Base Address Register
+//	unsigned int 	 idst;          //(0x88) SMC IDMAC Status Register
+//	unsigned int 	 idie;          //(0x8C) SMC IDMAC Interrupt Enable Register
+//	unsigned int 	 chda;          //(0x90)
+//	unsigned int 	 cbda;          //(0x94)
+//	unsigned int 	 res0[26];		//(0x98~0xff)
+//	unsigned int 	 fifo;          //(0x100) SMC FIFO Access Address
+};
+
+struct swmmc_host {
+	struct sw_mmc *reg;
+	unsigned mmc_no;
+	unsigned hclkbase;
+	unsigned mclkbase;
+	unsigned database;
+	unsigned fatal_err;
+};
+
+#ifdef SUNXI_MMCDBG
+#define MMCDBG(fmt...)	printf("[mmc]: "fmt)
+static void dumphex32(char* name, char* base, int len)
+{
+	__u32 i;
+	
+	printf("dump %s registers:", name);
+	for (i=0; i<len; i+=4)
+	{
+		if (!(i&0xf))
+			printf("\n0x%p : ", base + i);
+		printf("0x%08x ", readl(base + i));
+	}
+	printf("\n");
+}
+static void dumpmmcreg(struct sw_mmc *reg)
+{
+	printf("gctrl     0x%08x\n", reg->gctrl     );
+	printf("clkcr     0x%08x\n", reg->clkcr     );
+	printf("timeout   0x%08x\n", reg->timeout   );
+	printf("width     0x%08x\n", reg->width     );
+	printf("blksz     0x%08x\n", reg->blksz     );
+	printf("bytecnt   0x%08x\n", reg->bytecnt   );
+	printf("cmd       0x%08x\n", reg->cmd       );
+	printf("arg       0x%08x\n", reg->arg       );
+	printf("resp0     0x%08x\n", reg->resp0     );
+	printf("resp1     0x%08x\n", reg->resp1     );
+	printf("resp2     0x%08x\n", reg->resp2     );
+	printf("resp3     0x%08x\n", reg->resp3     );
+	printf("imask     0x%08x\n", reg->imask     );
+	printf("mint      0x%08x\n", reg->mint      );
+	printf("rint      0x%08x\n", reg->rint      );
+	printf("status    0x%08x\n", reg->status    );
+	printf("ftrglevel 0x%08x\n", reg->ftrglevel );
+	printf("funcsel   0x%08x\n", reg->funcsel   );
+}
+#else
+#define MMCDBG(fmt...)
+#define dumphex32(fmt...)
+#define dumpmmcreg(fmt...)
+#endif
+/* support 4 mmc hosts */
+struct mmc mmc_dev[4];
+struct swmmc_host mmc_host[4];
+
+static int swmmc_res_init(int sdc_no)
+{
+	struct swmmc_host* mmchost = &mmc_host[sdc_no];
+	MMCDBG("init mmc %d resource\n", sdc_no);
+	switch (sdc_no) {
+		case 0:
+			mmchost->reg = (struct sw_mmc *)SUNXI_SDMMC0_BASE;
+			mmchost->mclkbase = SUNXI_CCM_MMC0_SCLK_CFG;
+			break;
+		case 1:
+			mmchost->reg = (struct sw_mmc *)SUNXI_SDMMC1_BASE;
+			mmchost->mclkbase = SUNXI_CCM_MMC1_SCLK_CFG;
+			break;
+		case 2:
+			mmchost->reg = (struct sw_mmc *)SUNXI_SDMMC2_BASE;
+			mmchost->mclkbase = SUNXI_CCM_MMC2_SCLK_CFG;
+			break;
+		case 3:
+			mmchost->reg = (struct sw_mmc *)SUNXI_SDMMC3_BASE;
+			mmchost->mclkbase = SUNXI_CCM_MMC3_SCLK_CFG;
+			break;
+		default:
+			MMCDBG("Wrong mmc number %d\n", sdc_no);
+	}
+	mmchost->hclkbase = SUNXI_CCM_AHB_GATING0;
+	mmchost->database = (unsigned int)mmchost->reg + 0x100;
+	mmchost->mmc_no = sdc_no;
+	
+	return 0;
+}
+
+static int swmmc_clk_io_on(int sdc_no)
+{
+	unsigned int rval;
+	struct swmmc_host* mmchost = &mmc_host[sdc_no];
+	static struct sunxi_gpio *gpio_c =
+			&((struct sunxi_gpio_reg *)SUNXI_PIO_BASE)->gpio_bank[SUNXI_GPIO_C];
+	static struct sunxi_gpio *gpio_f =
+			&((struct sunxi_gpio_reg *)SUNXI_PIO_BASE)->gpio_bank[SUNXI_GPIO_F];
+	#if 0
+	static struct sunxi_gpio *gpio_g =
+			&((struct sunxi_gpio_reg *)SUNXI_PIO_BASE)->gpio_bank[SUNXI_GPIO_G];
+	#endif
+	static struct sunxi_gpio *gpio_h =
+			&((struct sunxi_gpio_reg *)SUNXI_PIO_BASE)->gpio_bank[SUNXI_GPIO_H];
+	static struct sunxi_gpio *gpio_i =
+			&((struct sunxi_gpio_reg *)SUNXI_PIO_BASE)->gpio_bank[SUNXI_GPIO_I];
+	MMCDBG("init mmc %d clock and io\n", sdc_no);
+	//config io
+	switch (sdc_no) {
+        case 0:
+            //D1-PF0, D0-PF1, CLK-PF2, CMD-PF3, D3-PF4, D4-PF5
+            writel(0x222222, &gpio_f->cfg[0]);
+            writel(0x555, &gpio_f->pull[0]);
+            writel(0x555, &gpio_f->drv[0]);
+            break;
+
+        case 1:
+            #if 0
+            //PG0-CMD, PG1-CLK, PG2~5-D0~3 : 4
+            writel(0x444444, &gpio_g->cfg[0]);
+            writel(0x555, &gpio_g->pull[0]);
+            writel(0x555, &gpio_g->drv[0]);
+            #else
+            //PH22-CMD, PH23-CLK, PH24~27-D0~D3 : 5
+            writel(0x55<<24, &gpio_h->cfg[2]);
+            writel(0x5555, &gpio_h->cfg[3]);
+            writel(0x555<<12, &gpio_h->pull[1]);
+            writel(0x555<<12, &gpio_h->drv[1]);
+            #endif
+            break;
+
+        case 2:
+            //CMD-PC6, CLK-PC7, D0-PC8, D1-PC9, D2-PC10, D3-PC11
+            writel(0x33<<24, &gpio_c->cfg[0]);
+            writel(0x3333, &gpio_c->cfg[1]);
+            writel(0x555<<12, &gpio_c->pull[0]);
+            writel(0x555<<12, &gpio_c->drv[0]);
+            break;
+
+        case 3:
+            //PI4-CMD, PI5-CLK, PI6~9-D0~D3 : 2
+            writel(0x2222<<16, &gpio_i->cfg[0]);
+            writel(0x22, &gpio_i->cfg[1]);
+            writel(0x555<<8, &gpio_i->pull[0]);
+            writel(0x555<<8, &gpio_i->drv[0]);
+            break;
+
+        default:
+            return -1;
+	}
+
+	//config clock
+	rval = readl(mmchost->hclkbase);
+	rval |= (1 << (8 + sdc_no));
+	writel(rval, mmchost->hclkbase);
+	writel(1U << 31, mmchost->mclkbase);
+	
+	dumphex32("ccmu", SUNXI_CCM_BASE, 0x100);
+	dumphex32("gpio", SUNXI_PIO_BASE, 0x100);
+	dumphex32("mmc", mmchost->reg, 0x100);
+    return 0;
+}
+
+static int mmc_update_clk(struct mmc *mmc)
+{
+	struct swmmc_host* mmchost = (struct swmmc_host *)mmc->priv;
+	unsigned int cmd;
+	unsigned timeout = 0xfffff;
+
+	cmd = (1U << 31) | (1 << 21) | (1 << 13);
+  	writel(cmd, &mmchost->reg->cmd);
+	while((readl(&mmchost->reg->cmd)&0x80000000) && timeout--);
+	if (!timeout)
+		return -1;
+
+	writel(readl(&mmchost->reg->rint), &mmchost->reg->rint);
+	return 0;
+}
+
+static int mmc_config_clock(struct mmc *mmc, unsigned div)
+{
+	struct swmmc_host* mmchost = (struct swmmc_host *)mmc->priv;
+	unsigned rval = readl(&mmchost->reg->clkcr);
+
+	/*
+	 * CLKCREG[7:0]: divider
+	 * CLKCREG[16]:  on/off
+	 * CLKCREG[17]:  power save
+	 */
+	/* Disable Clock */
+	rval &= ~(1 << 16);
+	writel(rval, &mmchost->reg->clkcr);
+	if(mmc_update_clk(mmc))
+		return -1;
+	/* Change Divider Factor */
+	rval &= ~(0xFF);
+	rval |= div;
+	writel(rval, &mmchost->reg->clkcr);
+	if(mmc_update_clk(mmc))
+		return -1;
+	/* Re-enable Clock */
+	rval |= (1 << 16);
+	writel(rval, &mmchost->reg->clkcr);
+	if(mmc_update_clk(mmc))
+		return -1;
+	return 0;
+}
+
+static void mmc_set_ios(struct mmc *mmc)
+{
+	struct swmmc_host* mmchost = (struct swmmc_host *)mmc->priv;
+	unsigned int clkdiv = 0;
+
+	MMCDBG("set ios: bus_width: %x, clock: %d\n", mmc->bus_width, mmc->clock);
+
+	/* Change clock first */
+	if (mmc->clock <= 400000)
+		clkdiv = (24000000 + (mmc->clock>>1))/mmc->clock/2;
+	if (mmc->clock)
+		if (mmc_config_clock(mmc, clkdiv)) {
+			mmchost->fatal_err = 1;
+			return;
+		}
+	/* Change bus width */
+	if (mmc->bus_width == 8)
+		writel(2, &mmchost->reg->width);
+	else if (mmc->bus_width == 4)
+		writel(1, &mmchost->reg->width);
+	else
+		writel(0, &mmchost->reg->width);
+}
+
+static int mmc_core_init(struct mmc *mmc)
+{
+	struct swmmc_host* mmchost = (struct swmmc_host *)mmc->priv;
+	/* reset controller */
+	writel(0x7, &mmchost->reg->gctrl);
+	return 0;
+}
+
+static int mmc_send_cmd(struct mmc *mmc, struct mmc_cmd *cmd,
+			struct mmc_data *data)
+{
+	struct swmmc_host* mmchost = (struct swmmc_host *)mmc->priv;
+	unsigned int cmdval = 0x80000000;
+	signed int timeout = 0;
+	unsigned int error = 0;
+	unsigned int status = 0;
+
+	if (mmchost->fatal_err)
+		return -1;
+	if (cmd->resp_type & MMC_RSP_BUSY)
+		MMCDBG("mmc cmd %d check rsp busy\n", cmd->cmdidx);
+	if (cmd->cmdidx == 12)
+		return 0;
+	/*
+	 * CMDREG
+	 * CMD[5:0]	: Command index
+	 * CMD[6]	: Has response
+	 * CMD[7]	: Long response
+	 * CMD[8]	: Check response CRC
+	 * CMD[9]	: Has data
+	 * CMD[10]	: Write
+	 * CMD[11]	: Steam mode
+	 * CMD[12]	: Auto stop
+	 * CMD[13]	: Wait previous over
+	 * CMD[14]	: About cmd
+	 * CMD[15]	: Send initialization
+	 * CMD[21]	: Update clock
+	 * CMD[31]	: Load cmd
+	 */
+	if (!cmd->cmdidx)
+		cmdval |= (1 << 15);
+	if (cmd->resp_type & MMC_RSP_PRESENT)
+		cmdval |= (1 << 6);
+	if (cmd->resp_type & MMC_RSP_136)
+		cmdval |= (1 << 7);
+	if (cmd->resp_type & MMC_RSP_CRC)
+		cmdval |= (1 << 8);
+	if (data) {
+		if ((u32)data->dest & 0x3) {
+			error = -1;
+			goto out;
+		}
+		
+		cmdval |= (1 << 9) | (1 << 13);
+		if (data->flags & MMC_DATA_WRITE)
+			cmdval |= (1 << 10);
+		if (data->blocks > 1)
+			cmdval |= (1 << 12);
+		writel(data->blocksize, &mmchost->reg->blksz);
+		writel(data->blocks * data->blocksize, &mmchost->reg->bytecnt);
+		writel(1U << 31, &mmchost->reg->gctrl);
+	}
+
+	MMCDBG("mmc %d, cmd %d(0x%08x), arg 0x%08x\n", mmchost->mmc_no, cmd->cmdidx, cmdval|cmd->cmdidx, cmd->cmdarg);
+	writel(cmd->cmdarg, &mmchost->reg->arg);
+	writel(cmdval|cmd->cmdidx, &mmchost->reg->cmd);
+	
+	/*
+	 * transfer data and check status
+	 * STATREG[2] : FIFO empty
+	 * STATREG[3] : FIFO full
+	 */
+	if (data) {
+		unsigned int i;
+		unsigned int byte_cnt = data->blocksize * data->blocks;
+		unsigned int *buff;
+		timeout = 0xfffff;
+		
+		MMCDBG("with data %d bytes\n", byte_cnt);
+		if (data->flags & MMC_DATA_READ) {
+			buff = (unsigned int *)data->dest;
+		    for (i=0; i<(byte_cnt>>2); i++) {
+				while(--timeout && (readl(&mmchost->reg->status)&(1 << 2)))
+					if (timeout <= 0) goto dataout;
+				buff[i] = readl(mmchost->database);
+				timeout = 0xfffff;
+			}
+		} else {
+			buff = (unsigned int *)data->src;
+		    for (i=0; i<(byte_cnt>>2); i++) {
+				while(--timeout && (readl(&mmchost->reg->status)&(1 << 3)));
+					if (timeout <= 0) goto dataout;
+				writel(buff[i], mmchost->database);
+				timeout = 0xfffff;
+			}
+		}
+dataout:
+		if (timeout <= 0) {
+			error = readl(&mmchost->reg->rint) & 0xbfc2;
+			goto out;
+		}
+	}
+	
+	timeout = 0xfffff;
+	do {
+		status = readl(&mmchost->reg->rint);
+		if (!timeout-- || (status & 0xbfc2)) {
+			error = status & 0xbfc2;
+			MMCDBG("cmd timeout %x\n", error);
+			goto out;
+		}
+	} while (!(status&0x4));
+	
+	if (data) {
+		timeout = 0xffff;
+		unsigned done = 0;
+		do {
+			status = readl(&mmchost->reg->rint);
+			if (!timeout-- || (status & 0xbfc2)) {
+				error = status & 0xbfc2;
+				MMCDBG("data timeout %x\n", error);
+				goto out;
+			}
+			if (data->blocks > 1)
+				done = status & (1 << 14);
+			else
+				done = status & (1 << 3);
+		} while (!done);
+	}
+
+	if (cmd->resp_type & MMC_RSP_BUSY) {
+		timeout = 0xfffff;
+		do {
+			status = readl(&mmchost->reg->status);
+			if (!timeout--) {
+				error = -1;
+				MMCDBG("busy timeout\n");
+				goto out;
+			}
+		} while (status & (1 << 9));
+	}
+	if (cmd->resp_type & MMC_RSP_136) {
+		cmd->response[0] = readl(&mmchost->reg->resp3);
+		cmd->response[1] = readl(&mmchost->reg->resp2);
+		cmd->response[2] = readl(&mmchost->reg->resp1);
+		cmd->response[3] = readl(&mmchost->reg->resp0);
+		MMCDBG("mmc resp 0x%08x 0x%08x 0x%08x 0x%08x\n",
+			cmd->response[3], cmd->response[2],
+			cmd->response[1], cmd->response[0]);
+	} else {
+		cmd->response[0] = readl(&mmchost->reg->resp0);
+		MMCDBG("mmc resp 0x%08x\n", cmd->response[0]);
+	}
+out:
+	if (error) {
+		writel(0x7, &mmchost->reg->gctrl);
+		mmc_update_clk(mmc);
+		MMCDBG("mmc cmd %d err 0x%08x\n", cmd->cmdidx, error);
+	}
+	writel(0xffffffff, &mmchost->reg->rint);
+
+	if (error)
+		return -1;
+	else
+		return 0;
+}
+
+static int swmmc_init(int sdc_no)
+{
+	struct mmc *mmc;
+
+	printf(" mmc initialize %d\n", sdc_no);
+
+	memset(&mmc_dev[sdc_no], 0, sizeof(struct mmc));
+	memset(&mmc_host[sdc_no], 0, sizeof(struct swmmc_host));
+	mmc = &mmc_dev[sdc_no];
+
+	sprintf(mmc->name, "Allwinner SD/MMC");
+	mmc->priv = &mmc_host[sdc_no];
+	mmc->send_cmd = mmc_send_cmd;
+	mmc->set_ios = mmc_set_ios;
+	mmc->init = mmc_core_init;
+
+	mmc->voltages = MMC_VDD_32_33 | MMC_VDD_33_34;
+	mmc->host_caps = MMC_MODE_4BIT;
+	mmc->host_caps |= MMC_MODE_HS_52MHz | MMC_MODE_HS;
+
+	mmc->f_min = 400000;
+	mmc->f_max = 25000000;
+
+	swmmc_res_init(sdc_no);
+	swmmc_clk_io_on(sdc_no);
+
+	mmc_register(mmc);
+
+	return 0;
+}
+
+int board_mmc_init(bd_t *bis)
+{
+	swmmc_init(0);
+//	swmmc_init(1);
+	return 0;
+}
