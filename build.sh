08e8b4a3f7d3d0db6fc8ac8c7470c7d1408d5cd9
Tom Cubie <tangliang@allwinnertech.com>
2011-12-22 15:14:18 +0800

    sunxi: add build script to build sunxi

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+PLATFORM=""
+MODULE=""
+
+show_help()
+{
+	printf "\nbuild.sh - Top level build scritps\n"
+	echo "Valid Options:"
+	echo "  -h  Show help message"
+	echo "  -p <platform> platform, e.g. sun4i, sun4i-lite, sun4i_crane"
+	printf "  -m <module> module\n\n"
+}
+
+while getopts hp:m: OPTION
+do
+	case $OPTION in
+	h) show_help
+	;;
+	p) PLATFORM=$OPTARG
+	;;
+	m) MODULE=$OPTARG
+	;;
+	*) show_help
+	;;
+esac
+done
+
+if [ -z "$PLATFORM" ]; then
+	show_help
+	exit 1
+fi
+
+if [ -z "$MODULE" ]; then
+	MODULE="all"
+fi
+
+make distclean && make -j4 $PLATFORM CROSS_COMPILE=arm-none-linux-gnueabi-
