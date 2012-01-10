cb44509e1e62a7cf19a12df4edda42072765efb4
Tom Cubie <tangliang@allwinnertech.com>
2012-01-10 17:20:22 +0800

    sunxi: change command prompt to sunxi

--- include/configs/sun5i_a12.h
+++ include/configs/sun5i_a12.h
@@ -120,7 +120,7 @@
 #define CONFIG_SYS_LONGHELP				/* undef to save memory */
 #define CONFIG_SYS_HUSH_PARSER			/* use "hush" command parser	*/
 #define CONFIG_SYS_PROMPT_HUSH_PS2	"> "
-#define CONFIG_SYS_PROMPT		"sun5i#"
+#define CONFIG_SYS_PROMPT		"sunxi#"
 #define CONFIG_SYS_CBSIZE	256			/* Console I/O Buffer Size */
 #define CONFIG_SYS_PBSIZE	384			/* Print Buffer Size */
 #define CONFIG_SYS_MAXARGS	16			/* max number of command args */
