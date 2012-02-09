85286157df38654b02e4e092ef2d79f11216d27c
Tom Cubie <tangliang@allwinnertech.com>
2012-02-09 09:25:47 +0800

    sunxi: change ccm register definition to c style

--- arch/arm/cpu/armv7/sunxi/board.c
+++ arch/arm/cpu/armv7/sunxi/board.c
@@ -57,8 +57,8 @@ sunxi_boot_type_t boot_from(void) {
 	return SUNXI_BOOT_TYPE_NULL;
 }
 
-int watchdog_init(void)
-{
+int watchdog_init(void) {
+
 	struct sunxi_wdog *wdog =
 		&((struct sunxi_timer_reg *)SUNXI_TIMER_BASE)->wdog;
 	/* disable watchdog */
@@ -67,30 +67,32 @@ int watchdog_init(void)
 	return 0;
 }
 
-int clock_init(void)
-{
+int clock_init(void) {
+
+	struct sunxi_ccm_reg *ccm =
+		(struct sunxi_ccm_reg *)SUNXI_CCM_BASE;
+
 	/* set clock source to OSC24M */
-	sr32((u32 *)SUNXI_CCM_CPU_AHB_APB0_CFG, 16, 2, CPU_CLK_SRC_OSC24M);		/* CPU_CLK_SRC_SEL [17:16] */
+	sr32(&ccm->cpu_ahb_apb0_cfg, 16, 2, CPU_CLK_SRC_OSC24M);		/* CPU_CLK_SRC_SEL [17:16] */
 
 	/* set the pll1 factors, pll1 out = 24MHz*n*k/m/p */	
-
-	sr32((u32 *)SUNXI_CCM_PLL1_CFG, 8, 5, PLL1_FACTOR_N);		/* PLL1_FACTOR_N [12:8] */
-	sr32((u32 *)SUNXI_CCM_PLL1_CFG, 4, 2, PLL1_FACTOR_K);		/* PLL1_FACTOR_K [5:4] */
-	sr32((u32 *)SUNXI_CCM_PLL1_CFG, 0, 2, PLL1_FACTOR_M);		/* PLL1_FACTOR_M [1:0] */
-	sr32((u32 *)SUNXI_CCM_PLL1_CFG, 16, 2, PLL1_FACTOR_P);		/* PLL1_FACTOR_P [17:16] */
+	sr32(&ccm->pll1_cfg, 8, 5, PLL1_FACTOR_N);		/* PLL1_FACTOR_N [12:8] */
+	sr32(&ccm->pll1_cfg, 4, 2, PLL1_FACTOR_K);		/* PLL1_FACTOR_K [5:4] */
+	sr32(&ccm->pll1_cfg, 0, 2, PLL1_FACTOR_M);		/* PLL1_FACTOR_M [1:0] */
+	sr32(&ccm->pll1_cfg, 16, 2, PLL1_FACTOR_P);		/* PLL1_FACTOR_P [17:16] */
 
 	/* wait for clock to be stable*/	
 	sdelay(0x4000);
 	/* set clock divider, cpu:axi:ahb:apb0 = 8:4:2:1 */
-	sr32((u32 *)SUNXI_CCM_CPU_AHB_APB0_CFG, 0, 2, AXI_DIV);	/* AXI_CLK_DIV_RATIO [1:0] */
+	sr32(&ccm->pll1_cfg, 0, 2, AXI_DIV);	/* AXI_CLK_DIV_RATIO [1:0] */
 #ifdef CONFIG_SUN5I
-	sr32((u32 *)SUNXI_CCM_CPU_AHB_APB0_CFG, 6, 2, AHB_CLK_SRC_AXI);/* AHB_CLK_SRC [7:6] */
+	sr32(&ccm->pll1_cfg, 6, 2, AHB_CLK_SRC_AXI);/* AHB_CLK_SRC [7:6] */
 #endif
-	sr32((u32 *)SUNXI_CCM_CPU_AHB_APB0_CFG, 4, 2, AHB_DIV);	/* AHB_CLK_DIV_RATIO [5:4] */
-	sr32((u32 *)SUNXI_CCM_CPU_AHB_APB0_CFG, 9, 2, APB0_DIV);	/* APB0_CLK_DIV_RATIO [9:8] */
+	sr32(&ccm->pll1_cfg, 4, 2, AHB_DIV);	/* AHB_CLK_DIV_RATIO [5:4] */
+	sr32(&ccm->pll1_cfg, 9, 2, APB0_DIV);	/* APB0_CLK_DIV_RATIO [9:8] */
 
 	/* change cpu clock source to pll1 */
-	sr32((u32 *)SUNXI_CCM_CPU_AHB_APB0_CFG, 16, 2, CPU_CLK_SRC_PLL1);/* CPU_CLK_SRC_SEL [17:16] */
+	sr32(&ccm->pll1_cfg, 16, 2, CPU_CLK_SRC_PLL1);/* CPU_CLK_SRC_SEL [17:16] */
 	/* 
 	 * if the clock source is changed,
 	 * at most wait for 8 present running clock cycles
@@ -98,9 +100,9 @@ int clock_init(void)
 	sdelay(10);
 
 	/* config apb1 clock */
-	sr32((u32 *)SUNXI_CCM_APB1_CLK_DIV, 24, 2, APB1_CLK_SRC_OSC24M);
-	sr32((u32 *)SUNXI_CCM_APB1_CLK_DIV, 16, 2, APB1_FACTOR_N);
-	sr32((u32 *)SUNXI_CCM_APB1_CLK_DIV, 0, 5, APB1_FACTOR_M);
+	sr32(&ccm->apb1_clk_div_cfg, 24, 2, APB1_CLK_SRC_OSC24M);
+	sr32(&ccm->apb1_clk_div_cfg, 16, 2, APB1_FACTOR_N);
+	sr32(&ccm->apb1_clk_div_cfg, 0, 5, APB1_FACTOR_M);
 
 	/* open the clock for uart0 */
 	sr32((u32 *)SUNXI_CCM_APB1_GATING, 16, 1, CLK_GATE_OPEN);
@@ -116,8 +118,8 @@ int clock_init(void)
 	return 0;
 }
 
-int gpio_init(void)
-{
+int gpio_init(void) {
+
 	u32 i;
 	static struct sunxi_gpio *gpio_c =
 		&((struct sunxi_gpio_reg *)SUNXI_PIO_BASE)->gpio_bank[SUNXI_GPIO_C];
@@ -132,24 +134,52 @@ int gpio_init(void)
 	return 0;
 }
 
+int sdram_init(void) {
+
+	return 0;
+}
+
+u32 get_base(void) {
+
+	u32 val;
+
+	__asm__ __volatile__("mov %0, pc \n":"=r"(val)::"memory");
+	val &= 0xF0000000;
+	val >>= 28;
+	return val;
+}
+
+u32 is_running_in_sdram(void) {
+
+	if (get_base() > 4)
+		return 1;	/* in SDRAM */
+
+	return 0;		/* running in SRAM */
+}
+
 /* do some early init */
-void s_init(void)
-{
+void s_init(void) {
+
+	int in_sdram = is_running_in_sdram();
+
 	watchdog_init();
 	sunxi_key_init();
 	clock_init();
 	gpio_init();
+
+	if (!in_sdram)
+		sdram_init();
 }
 
-void reset_cpu(ulong addr)
-{
-	//sunxi_nand_flush_opts();
+extern int sunxi_reset(void);
+void reset_cpu(ulong addr) {
+
 	sunxi_reset();
 }
 
 #ifndef CONFIG_SYS_DCACHE_OFF
-void enable_caches(void)
-{
+void enable_caches(void) {
+
 	/* Enable D-cache. I-cache is already enabled in start.S */
 	dcache_enable();
 }
