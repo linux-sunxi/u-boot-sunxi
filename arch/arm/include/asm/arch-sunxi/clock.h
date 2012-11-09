df54f8e7c396c146a14070d46d2ace67f6ebce54
Henrik Nordstrom <henrik@henriknordstrom.net>
2012-11-09 10:45:45 +0100

    ARM sunxi: Kill obsolete clock constants of no added value

--- arch/arm/include/asm/arch-sunxi/clock.h
+++ arch/arm/include/asm/arch-sunxi/clock.h
@@ -27,40 +27,6 @@
 
 /* clock control module regs definition */
 
-// test-only: these defines need to be converted to a struct
-#define SUNXI_CCM_PLL1_CFG		(SUNXI_CCM_BASE + 0x00)
-#define SUNXI_CCM_PLL1_TUN		(SUNXI_CCM_BASE + 0x04)
-#define SUNXI_CCM_PLL2_CFG		(SUNXI_CCM_BASE + 0x08)
-#define SUNXI_CCM_PLL2_TUN		(SUNXI_CCM_BASE + 0x0C)
-#define SUNXI_CCM_PLL3_CFG		(SUNXI_CCM_BASE + 0x10)
-#define SUNXI_CCM_PLL4_CFG		(SUNXI_CCM_BASE + 0x18)
-#define SUNXI_CCM_PLL5_CFG		(SUNXI_CCM_BASE + 0x20)
-#define SUNXI_CCM_PLL5_TUN		(SUNXI_CCM_BASE + 0x24)
-#define SUNXI_CCM_PLL6_CFG		(SUNXI_CCM_BASE + 0x28)
-#define SUNXI_CCM_PLL7_CFG		(SUNXI_CCM_BASE + 0x30)
-
-#define SUNXI_CCM_PLL1_TUN2		(SUNXI_CCM_BASE + 0x38)
-#define SUNXI_CCM_PLL5_TUN2		(SUNXI_CCM_BASE + 0x3C)
-
-#define SUNXI_CCM_PLL_LOCK_DBG		(SUNXI_CCM_BASE + 0x4C)
-
-#define SUNXI_CCM_OSC24M_CFG		(SUNXI_CCM_BASE + 0x50)
-#define SUNXI_CCM_CPU_AHB_APB0_CFG	(SUNXI_CCM_BASE + 0x54)
-#define SUNXI_CCM_APB1_CLK_DIV		(SUNXI_CCM_BASE + 0x58)
-#define SUNXI_CCM_AXI_GATING		(SUNXI_CCM_BASE + 0x5C)
-#define SUNXI_CCM_AHB_GATING0		(SUNXI_CCM_BASE + 0x60)
-#define SUNXI_CCM_AHB_GATING1		(SUNXI_CCM_BASE + 0x64)
-#define SUNXI_CCM_APB0_GATING		(SUNXI_CCM_BASE + 0x68)
-#define SUNXI_CCM_APB1_GATING		(SUNXI_CCM_BASE + 0x6C)
-
-#define SUNXI_CCM_NAND_SCLK_CFG		(SUNXI_CCM_BASE + 0x80)
-
-#define SUNXI_CCM_MMC0_SCLK_CFG		(SUNXI_CCM_BASE + 0x88)
-#define SUNXI_CCM_MMC1_SCLK_CFG		(SUNXI_CCM_BASE + 0x8C)
-#define SUNXI_CCM_MMC2_SCLK_CFG		(SUNXI_CCM_BASE + 0x90)
-#define SUNXI_CCM_MMC3_SCLK_CFG		(SUNXI_CCM_BASE + 0x94)
-
-// test-only: is this already the struct? then use it and remove the defines above
 struct sunxi_ccm_reg {
 	u32 pll1_cfg;		/* 0x00 pll1 control */
 	u32 pll1_tun;		/* 0x04 pll1 tuning */
@@ -138,29 +104,10 @@ struct sunxi_ccm_reg {
 	u32 mbus_clk_cfg;	/* 0x15c */
 };
 
-/* pll1 factors */
-#define PLL1_FACTOR_N			21
-#define PLL1_FACTOR_K			1
-#define PLL1_FACTOR_M			0
-#define PLL1_FACTOR_P			0
-#define PLL1_ENABLE			1
-
 /* apb1 bit field */
 #define APB1_CLK_SRC_OSC24M		0
 #define APB1_FACTOR_M			0
 #define APB1_FACTOR_N			0
-
-/* pll5(for ddr) bit field */
-#define DDR_CLK_HZ			(408 * 1024 * 1024)
-#define OSC24M_CLK_HZ			(24 * 1024 * 1024)
-
-#define PLL5_FACTOR_N			(DDR_CLK_HZ / OSC24M_CLK_HZ)
-#define PLL5_FACTOR_K			1
-#define PLL5_FACTOR_M			1
-#define PLL5_OUT_DIV_P			1
-#define PLL5_ENABLE			1
-#define DDR_CLK_OUT_ENABLE		1
-#define DDR_CLK_OUT_DISABLE		0
 
 /* clock divide */
 #define CPU_CLK_SRC_OSC24M		1
