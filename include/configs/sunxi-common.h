455c2156d7e47788514a5ce314e65b303afdd4f4
Henrik Nordstrom <henrik@henriknordstrom.net>
2012-11-14 01:34:27 +0100

    sunxi: Increase SPL u-boot load size a bit

--- include/configs/sunxi-common.h
+++ include/configs/sunxi-common.h
@@ -143,7 +143,7 @@
  */
 #define CONFIG_SYS_NO_FLASH
 
-#define CONFIG_SYS_MONITOR_LEN		(256 << 10)	/* 256 KB */
+#define CONFIG_SYS_MONITOR_LEN		(512 << 10)	/* 512 KB */
 #define CONFIG_IDENT_STRING		" Allwinner Technology "
 
 #define CONFIG_ENV_OFFSET		(544 << 10) /* (8 + 24 + 512)KB */
