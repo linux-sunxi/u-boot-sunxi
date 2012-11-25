e05c1455148d9f5e319812c1c1b3f8b676076803
Henrik Nordstrom <henrik@henriknordstrom.net>
2012-11-25 11:15:21 +0100

    ARM sunxi: Cleanup AXP209 driver

--- drivers/power/axp209.c
+++ drivers/power/axp209.c
@@ -25,40 +25,40 @@
 #include <i2c.h>
 #include <axp209.h>
 
-typedef enum {
+enum axp209_reg {
 	AXP209_CHIP_VERSION = 0x3,
 	AXP209_DCDC2_VOLTAGE = 0x23,
 	AXP209_DCDC3_VOLTAGE = 0x27,
 	AXP209_LDO24_VOLTAGE = 0x28,
 	AXP209_LDO3_VOLTAGE = 0x29,
 	AXP209_SHUTDOWN = 0x32,
-} axp209_reg;
+};
 
-int axp209_write(axp209_reg reg, u8 val)
+int axp209_write(enum axp209_reg reg, u8 val)
 {
 	return i2c_write(0x34, reg, 1, &val, 1);
 }
 
-int axp209_read(axp209_reg reg, u8 * val)
+int axp209_read(enum axp209_reg reg, u8 * val)
 {
 	return i2c_read(0x34, reg, 1, val, 1);
 }
 
 int axp209_set_dcdc2(int mvolt)
 {
-	int target = (mvolt - 700) / 25;
+	int cfg = (mvolt - 700) / 25;
 	int rc;
 	u8 current;
 
-	if (target < 0)
-		target = 0;
-	if (target > (1 << 6) - 1)
-		target = (1 << 6) - 1;
+	if (cfg < 0)
+		cfg = 0;
+	if (cfg > (1 << 6) - 1)
+		cfg = (1 << 6) - 1;
 
 	/* Do we really need to be this gentle? It has built-in voltage slope */
 	while ((rc = axp209_read(AXP209_DCDC2_VOLTAGE, &current)) == 0
-	       && current != target) {
-		if (current < target)
+	       && current != cfg) {
+		if (current < cfg)
 			current++;
 		else
 			current--;
@@ -73,16 +73,16 @@ int axp209_set_dcdc2(int mvolt)
 
 int axp209_set_dcdc3(int mvolt)
 {
-	int target = (mvolt - 700) / 25;
+	int cfg = (mvolt - 700) / 25;
 	u8 reg;
 	int rc;
 
-	if (target < 0)
-		target = 0;
-	if (target > (1 << 7) - 1)
-		target = (1 << 7) - 1;
+	if (cfg < 0)
+		cfg = 0;
+	if (cfg > (1 << 7) - 1)
+		cfg = (1 << 7) - 1;
 
-	rc = axp209_write(AXP209_DCDC3_VOLTAGE, target);
+	rc = axp209_write(AXP209_DCDC3_VOLTAGE, cfg);
 	rc |= axp209_read(AXP209_DCDC3_VOLTAGE, &reg);
 
 	return rc;
@@ -90,20 +90,20 @@ int axp209_set_dcdc3(int mvolt)
 
 int axp209_set_ldo2(int mvolt)
 {
-	int target = (mvolt - 1800) / 100;
+	int cfg = (mvolt - 1800) / 100;
 	int rc;
 	u8 reg;
 
-	if (target < 0)
-		target = 0;
-	if (target > 15)
-		target = 15;
+	if (cfg < 0)
+		cfg = 0;
+	if (cfg > 15)
+		cfg = 15;
 
 	rc = axp209_read(AXP209_LDO24_VOLTAGE, &reg);
 	if (rc)
 		return rc;
 
-	reg = (reg & 0x0f) | (target << 4);
+	reg = (reg & 0x0f) | (cfg << 4);
 	rc = axp209_write(AXP209_LDO24_VOLTAGE, reg);
 	if (rc)
 		return rc;
@@ -113,21 +113,21 @@ int axp209_set_ldo2(int mvolt)
 
 int axp209_set_ldo3(int mvolt)
 {
-	int target = (mvolt - 700) / 25;
+	int cfg = (mvolt - 700) / 25;
 
-	if (target < 0)
-		target = 0;
-	if (target > 127)
-		target = 127;
+	if (cfg < 0)
+		cfg = 0;
+	if (cfg > 127)
+		cfg = 127;
 	if (mvolt == -1)
-		target = 0x80;	/* detemined by LDO3IN pin */
+		cfg = 0x80;	/* detemined by LDO3IN pin */
 
-	return axp209_write(AXP209_LDO3_VOLTAGE, target);
+	return axp209_write(AXP209_LDO3_VOLTAGE, cfg);
 }
 
 int axp209_set_ldo4(int mvolt)
 {
-	int target = (mvolt - 1800) / 100;
+	int cfg = (mvolt - 1800) / 100;
 	int rc;
 	static const int vindex[] = {
 		1250, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2500,
@@ -135,15 +135,16 @@ int axp209_set_ldo4(int mvolt)
 	};
 	u8 reg;
 
-	/* Translate mvolt to register target value */
-	for (target = 0; mvolt < vindex[target] && target < 15; target++) {
-	}
+	/* Translate mvolt to register cfg value, requested <= selected */
+	for (cfg = 0; mvolt < vindex[cfg] && cfg < 15; cfg++)
+		;
 
 	rc = axp209_read(AXP209_LDO24_VOLTAGE, &reg);
 	if (rc)
 		return rc;
 
-	reg = (reg & 0xf0) | (target << 0);
+	/* LDO4 configuration is in lower 4 bits */
+	reg = (reg & 0xf0) | (cfg << 0);
 	rc = axp209_write(AXP209_LDO24_VOLTAGE, reg);
 	if (rc)
 		return rc;
