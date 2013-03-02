9179e66703aa3a5c0a4962e49613c59220c2af34
csjamesdeng <csjamesdeng@allwinnertech.com>
2013-03-02 10:58:10 +0800

    mmc: use inter-dma as default.

--- include/configs/sun7i.h
+++ include/configs/sun7i.h
@@ -103,7 +103,7 @@
 #define CONFIG_CMD_MMC
 #define CONFIG_MMC_SUNXI
 #define CONFIG_MMC_SUNXI_SLOT		2		/* which mmc slot to use, could be 0,1,2,3 */
-//#define CONFIG_MMC_SUNXI_USE_DMA
+#define CONFIG_MMC_SUNXI_USE_DMA
 #define CONFIG_ENV_IS_IN_MMC
 #define CONFIG_SYS_MMC_ENV_DEV		mmc_card_no
 #define CONFIG_STORAGE_EMMC
