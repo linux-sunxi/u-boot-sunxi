baa70c6d9af279b3d7566ff8d16d9728da724a5d
Jerry Wang <wangflord@allwinnertech.com>
2013-05-31 14:48:04 +0800

    License: fix licesne header for sunxi files
    Add GPL licesne header
    
    Signed-off-by: Jerry Wang <wangflord@allwinnertech.com>

--- arch/arm/include/asm/arch-sunxi/nand_fspart.h
+++ arch/arm/include/asm/arch-sunxi/nand_fspart.h
@@ -1,18 +1,27 @@
 /*
-*********************************************************************************************************
-*											        eBase
-*
-*
-*
-*						        (c) Copyright 2006-2010, AW China
-*											All	Rights Reserved
-*
-* File    	: 	egon_drv_common_inc.h
-* Date	:	2010-09-25
-* By      	: 	holigun
-* Version 	: 	V1.00
-*********************************************************************************************************
-*/
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * Jerry Wang <wangflord@allwinnertech.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
 #ifndef	_NAND_MBR_H_
 #define	_NAND_MBR_H_
 
--- drivers/dma/sunxi_dma.c
+++ drivers/dma/sunxi_dma.c
@@ -1,23 +1,27 @@
 /*
-**********************************************************************************************************************
-*											        eGon
-*						           the Embedded GO-ON Bootloader System
-*									       eGON driver sub-system
-*
-*						  Copyright(C), 2006-2010, SoftWinners Microelectronic Co., Ltd.
-*                                           All Rights Reserved
-*
-* File    : dma.c
-*
-* By      : Jerry
-*
-* Version : V2.00
-*
-* Date	  :
-*
-* Descript:
-**********************************************************************************************************************
-*/
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * Jerry Wang <wangflord@allwinnertech.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
 #include <asm/arch/dma_i.h>
 #include <asm/arch/dma.h>
 #include <common.h>
