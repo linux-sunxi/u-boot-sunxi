a6dd18a4435652fc3e08e65da542f7c0f074752f
Hans de Goede <hdegoede@redhat.com>
2014-01-09 23:26:05 +0100

    sunxi: A10-OLinuXino-Lime support fixup
    Fix:
    -code indentation
    -dram timings, the used cas and tpr0 - tpr2 were typical sun5i / sun7i
     settings, while they should be sun4i settings
    -Properly sort boards.cfg entry
    -Add status led support
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    
    Conflicts:
    	boards.cfg

--- board/sunxi/dram_a10_olinuxino_l.c
+++ board/sunxi/dram_a10_olinuxino_l.c
@@ -4,28 +4,28 @@
 #include <asm/arch/dram.h>
 
 static struct dram_para dram_para = {
-       .clock = 480,
-       .type = 3,
-       .rank_num = 1,
-       .density = 4096,
-       .io_width = 16,
-       .bus_width = 16,
-       .cas = 9,
-       .zq = 123,
-       .odt_en = 0,
-       .size = 512,
-       .tpr0 = 0x42d899b7,
-       .tpr1 = 0xa090,
-       .tpr2 = 0x22a00,
-       .tpr3 = 0,
-       .tpr4 = 0,
-       .tpr5 = 0,
-       .emr1 = 0x4,
-       .emr2 = 0x10,
-       .emr3 = 0,
+	.clock = 480,
+	.type = 3,
+	.rank_num = 1,
+	.density = 4096,
+	.io_width = 16,
+	.bus_width = 16,
+	.cas = 6,
+	.zq = 123,
+	.odt_en = 0,
+	.size = 512,
+	.tpr0 = 0x30926692,
+	.tpr1 = 0x1090,
+	.tpr2 = 0x1a0c8,
+	.tpr3 = 0,
+	.tpr4 = 0,
+	.tpr5 = 0,
+	.emr1 = 0x4,
+	.emr2 = 0,
+	.emr3 = 0,
 };
 
 unsigned long sunxi_dram_init(void)
 {
-       return dramc_init(&dram_para);
+	return dramc_init(&dram_para);
 }
