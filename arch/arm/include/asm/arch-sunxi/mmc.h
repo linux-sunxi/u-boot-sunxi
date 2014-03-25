2cab22f87a16f5f0896f7c9bbe8c716aea1919f5
Olliver Schinagl <oliver@schinagl.nl>
2014-03-25 22:17:42 +0100

    Make mmc header comments consistent with the rest
    This small patch makes the comments in the mmc driver consistent with
    the rest. As reference clock.h was used.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- arch/arm/include/asm/arch-sunxi/mmc.h
+++ arch/arm/include/asm/arch-sunxi/mmc.h
@@ -14,36 +14,36 @@
 #include <linux/types.h>
 
 struct sunxi_mmc {
-	u32 gctrl;		/* (0x00) SMC Global Control Register */
-	u32 clkcr;		/* (0x04) SMC Clock Control Register */
-	u32 timeout;		/* (0x08) SMC Time Out Register */
-	u32 width;		/* (0x0c) SMC Bus Width Register */
-	u32 blksz;		/* (0x10) SMC Block Size Register */
-	u32 bytecnt;		/* (0x14) SMC Byte Count Register */
-	u32 cmd;		/* (0x18) SMC Command Register */
-	u32 arg;		/* (0x1c) SMC Argument Register */
-	u32 resp0;		/* (0x20) SMC Response Register 0 */
-	u32 resp1;		/* (0x24) SMC Response Register 1 */
-	u32 resp2;		/* (0x28) SMC Response Register 2 */
-	u32 resp3;		/* (0x2c) SMC Response Register 3 */
-	u32 imask;		/* (0x30) SMC Interrupt Mask Register */
-	u32 mint;		/* (0x34) SMC Masked Interrupt Status Reg */
-	u32 rint;		/* (0x38) SMC Raw Interrupt Status Register */
-	u32 status;		/* (0x3c) SMC Status Register */
-	u32 ftrglevel;		/* (0x40) SMC FIFO Threshold Watermark Reg */
-	u32 funcsel;		/* (0x44) SMC Function Select Register */
-	u32 cbcr;		/* (0x48) SMC CIU Byte Count Register */
-	u32 bbcr;		/* (0x4c) SMC BIU Byte Count Register */
-	u32 dbgc;		/* (0x50) SMC Debug Enable Register */
-	u32 res0[11];		/* (0x54~0x7c) */
-	u32 dmac;		/* (0x80) SMC IDMAC Control Register */
-	u32 dlba;		/* (0x84) SMC IDMAC Descr List Base Addr Reg */
-	u32 idst;		/* (0x88) SMC IDMAC Status Register */
-	u32 idie;		/* (0x8c) SMC IDMAC Interrupt Enable Register */
-	u32 chda;		/* (0x90) */
-	u32 cbda;		/* (0x94) */
-	u32 res1[26];		/* (0x98~0xff) */
-	u32 fifo;		/* (0x100) SMC FIFO Access Address */
+	u32 gctrl;		/* 0x00 global control */
+	u32 clkcr;		/* 0x04 clock control */
+	u32 timeout;		/* 0x08 time out */
+	u32 width;		/* 0x0c bus width */
+	u32 blksz;		/* 0x10 block size */
+	u32 bytecnt;		/* 0x14 byte count */
+	u32 cmd;		/* 0x18 command */
+	u32 arg;		/* 0x1c argument */
+	u32 resp0;		/* 0x20 response 0 */
+	u32 resp1;		/* 0x24 response 1 */
+	u32 resp2;		/* 0x28 response 2 */
+	u32 resp3;		/* 0x2c response 3 */
+	u32 imask;		/* 0x30 interrupt mask */
+	u32 mint;		/* 0x34 masked interrupt status */
+	u32 rint;		/* 0x38 raw interrupt status */
+	u32 status;		/* 0x3c status */
+	u32 ftrglevel;		/* 0x40 FIFO threshold watermark*/
+	u32 funcsel;		/* 0x44 function select */
+	u32 cbcr;		/* 0x48 CIU byte count */
+	u32 bbcr;		/* 0x4c BIU byte count */
+	u32 dbgc;		/* 0x50 debug enable */
+	u32 res0[11];
+	u32 dmac;		/* 0x80 internal DMA control */
+	u32 dlba;		/* 0x84 internal DMA descr list base address */
+	u32 idst;		/* 0x88 internal DMA status */
+	u32 idie;		/* 0x8c internal DMA interrupt enable */
+	u32 chda;		/* 0x90 */
+	u32 cbda;		/* 0x94 */
+	u32 res1[26];
+	u32 fifo;		/* 0x100 FIFO access address */
 };
 
 #define SUNXI_MMC_CLK_POWERSAVE		(0x1 << 17)
