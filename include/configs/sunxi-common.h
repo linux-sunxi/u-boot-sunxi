87ca6dc0262d18b76e4749fc6505ef596039656a
Hans de Goede <hdegoede@redhat.com>
2014-04-08 22:48:44 +0200

    sunxi: Use generic board setup
    Switch from the old per arch board config to the new generic config,
    the old config is deprecated and will be removed in the near future.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- include/configs/sunxi-common.h
+++ include/configs/sunxi-common.h
@@ -109,6 +109,7 @@
 #define CONFIG_SYS_CBSIZE	256	/* Console I/O Buffer Size */
 #define CONFIG_SYS_PBSIZE	384	/* Print Buffer Size */
 #define CONFIG_SYS_MAXARGS	16	/* max number of command args */
+#define CONFIG_SYS_GENERIC_BOARD
 
 /* Boot Argument Buffer Size */
 #define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE
