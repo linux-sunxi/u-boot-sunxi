acf80d2841f055e489d81f6d7f400672bff0571e
Hans de Goede <hdegoede@redhat.com>
2014-05-29 23:45:04 +0200

    sun5i: Differentiate between A10s and A13
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- arch/arm/cpu/armv7/sunxi/cpu_info.c
+++ arch/arm/cpu/armv7/sunxi/cpu_info.c
@@ -16,8 +16,13 @@ int print_cpuinfo(void)
 #ifdef CONFIG_SUN4I
 	puts("CPU:   Allwinner A10 (SUN4I)\n");
 #elif defined CONFIG_SUN5I
-	/* TODO: Distinguish A13/A10s */
-	puts("CPU:   Allwinner A13/A10s (SUN5I)\n");
+	u32 val = readl(SUNXI_SID_BASE + 0x08);
+	switch ((val >> 12) & 0xf) {
+	case 0: puts("CPU:   Allwinner A12 (SUN5I)\n"); break;
+	case 3: puts("CPU:   Allwinner A13 (SUN5I)\n"); break;
+	case 7: puts("CPU:   Allwinner A10s (SUN5I)\n"); break;
+	default: puts("CPU:   Allwinner A1X (SUN5I)\n");
+	}
 #elif defined CONFIG_SUN6I
 	puts("CPU:   Allwinner A31 (SUN6I)\n");
 #elif defined CONFIG_SUN7I
