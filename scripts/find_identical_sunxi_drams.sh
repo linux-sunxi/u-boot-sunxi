bf9b23f3e88a8b4ace3d154966de59f5866c0c1e
Julian Calaby <julian.calaby@gmail.com>
2014-09-24 15:48:26 +0200

    scripts: add script to check for duplicate sunxi dram files
    Signed-off-by: Julian Calaby <julian.calaby@gmail.com>

---
+++ scripts/find_identical_sunxi_drams.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TMPFILE=$(tempfile)
+
+find -wholename '*/sunxi/dram_*.c' \
+       | while read r; do
+		echo -n "$r:";
+		# Rip out dram params
+		cat $r \
+			| sed -n '/^\s*static struct/,/^};/p' \
+			| grep '^\s*\.' \
+			| sed 's/,.*$//;s/^\s*//' \
+			| sort \
+			| while read a b c; do
+				echo $a $b $((c));
+			done \
+			| tr -d '\n'; #concatenate
+		echo;
+	done \
+	| tee $TMPFILE \
+	| sed 's/^.*://' \
+	| sort \
+	| uniq -c \
+	| grep -v '^\s*1\s' \
+	| while read c r; do
+		echo "Found a set of identical sunxi dram files:";
+		grep -F "$r" $TMPFILE \
+			| sed 's/:.*//';
+		echo
+	done;
+
+rm -f $TMPFILE;
